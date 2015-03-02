301d2eaf67a7ff1e2b549641467b623be6384a70
Ben Hutchison <ben@aldaviva.com>
2015-03-01 21:47:24 -0800

    Resolves #4: list twitter handle and the timestamp below the tweet

--- www/scripts/twitter.js
+++ www/scripts/twitter.js
@@ -3,9 +3,17 @@
 	var USERNAME = "maddangerous";
 
 	var twitterEl = $('.twitter');
+	var metadataEl = $('.tweet_metadata', twitterEl);
+	metadataEl.hide();
 
 	$.getJSON("https://aldaviva.com/portfolio/data/social/twitter/"+USERNAME, function(twitterStatus){
 		$('.tweet_body', twitterEl).text(twitterStatus.body);
+
+		var tweetMoment = new moment(twitterStatus.created);
+		$('.time', metadataEl).text(tweetMoment.format("MMM D, YYYY"));
+		$('.date', metadataEl).text(tweetMoment.format("h:mm a"));
+		metadataEl.show();
+
 	});
 
 })();
