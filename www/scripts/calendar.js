3fb1d204fc8e68a0fb341b366a1ad17c6be77f16
Ben Hutchison <ben@aldaviva.com>
2015-03-01 20:52:26 -0800

    Resolves #1: remove calendar part

--- www/scripts/calendar.js
+++ www/scripts/calendar.js
@@ -1,16 +1,23 @@
 (function(){
 
-	var CALENDAR_ID = ""; //TODO get calendar id from will
+	var CALENDAR_ID = "avmde75fseoi8qsem9rde92hu0@group.calendar.google.com"; //TODO get calendar id from will
 
 	var calendarEl = $('.calendar');
 
-	$.getJSON("https://aldaviva.com/portfolio/data/social/calendar", function(nextEvent){
-		$('.title', calendarEl).text(nextEvent.title);
-		var eventDate = new Date(nextEvent.startTime);
-		$('.date_location', calendarEl).text(nextEvent.location + ', '
-			+ (eventDate.getMonth()+1)+'/'+eventDate.getDate() + ' @ '
-			+ eventDate.getHours()%12 + (eventDate.getMinutes() === 0 ? '' : ':'+eventDate.getMinutes()) + (eventDate.getHours<12 ? 'AM' : 'PM'));
-		$('.addevent', calendarEl).attr('href', nextEvent.addEventUrl);
-	});
+	calendarEl.hide();
+
+	$.getJSON("https://aldaviva.com/portfolio/data/social/googlecalendar/"+CALENDAR_ID)
+		.done(function(nextEvent){
+			$('.title', calendarEl).text(nextEvent.title);
+			var eventDate = new Date(nextEvent.startTime);
+			$('.date_location', calendarEl).text(nextEvent.location || 'location tbd' + ', '
+				+ (eventDate.getMonth()+1)+'/'+eventDate.getDate() + ' @ '
+				+ eventDate.getHours()%12 + (eventDate.getMinutes() === 0 ? '' : ':'+eventDate.getMinutes()) + (eventDate.getHours<12 ? 'AM' : 'PM'));
+			$('.addevent', calendarEl).attr('href', nextEvent.addEventUrl);
+			calendarEl.show();
+		})
+		.fail(function(err){
+			calendarEl.hide();
+		});
 
 })();
